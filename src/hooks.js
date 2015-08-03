f0f65c8ad563f6357511517797560c38d9387358
Zoilorys <zoilorys@gmail.com>
2015-08-03 19:52:38 +0300

    updated hooks

--- src/hooks.js
+++ src/hooks.js
@@ -1,8 +1,74 @@
+export function hooksManager() {
+  const hooks = {
+    create: {
+      before: [
+        function(value) {
+          return value;
+        }
+      ],
+      after: [
+        function(value) {
+          return value;
+        }
+      ],
+    },
+    update: {
+      before: [
+        function(value) {
+          return value;
+        }
+      ],
+      after: [
+        function(value) {
+          return value;
+        }
+      ],
+    },
+    find: {
+      before: [
+        function(value) {
+          return value;
+        }
+      ],
+      after: [
+        function(value) {
+          return value;
+        }
+      ],
+    },
+    delete: {
+      before: [
+        function(value) {
+        return value;
+        }
+      ],
+      after: [
+        function(value) {
+          return value;
+        }
+      ],
+    },
+  };
 
-export function registerHook(hooks, name, hook) {
-  return hooks[name].push(hook);
-}
+  const controller = {};
 
-export function getHooks(hooks, name) {
-  return hooks[name];
-}
+  controller.registerHook = function(name, hook) {
+    const args = name.split(' ');
+    hooks[args[0]][args[1]].push(hook);
+    return this;
+  };
+
+  controller.execHooks = function(prefix) {
+    return function(suffix) {
+      return function(value) {
+        return hooks[prefix][suffix].reduce(function(value, func) {
+          return func(value);
+        }, value);
+      };
+    };
+  };
+
+  return controller;
+};
+
+export const hooks = hooksManager();
